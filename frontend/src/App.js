68dc688b1d0a37e3d7912062ce76a857a22b85bc
NaLeum <wognsdl@likelion.org>
2020-11-21 18:01:29 +0900

    init project

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import HomePage from './pages/HomePage/HomePage';
+
+
+const App = () => {
+  return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+    </Router>
+  );
+}
+
+export default App;
