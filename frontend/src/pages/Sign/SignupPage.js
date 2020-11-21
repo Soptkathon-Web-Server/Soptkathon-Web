a90f8c0562193ef1974ce80799443f1cb43f74b3
Say-young <seyoung990525@gmail.com>
2020-11-22 04:53:26 +0900

    로그인 퍼블리싱 1차

--- frontend/src/pages/Sign/SignupPage.js
+++ frontend/src/pages/Sign/SignupPage.js
@@ -1,23 +1,30 @@
-import './Login.scss'
+import './Signup.scss'
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-const LoginPage = () => {
+const SignupPage = () => {
   return (
     <div class = "outer">
-      <div class="container"></div>
+    <div class="container"></div>
         <div class = "left">
             <div class = "content">
-                <div class = "title">내 호수로 가기 위해, 로그인</div>
+                <div class = "title">오늘의 돌멩이를 나만의 호수에 쌓아보세요.</div>
+                <div class = "center">
                     <form class = "form">
                         <div><input class="inputbox" type="text" placeholder="호수의 이름"/></div>
                         <div><input class="inputbox" type="text" placeholder="비밀번호"/></div>
                         <div><input class="inputbox" type="text" placeholder="비밀번호 확인"/></div>
                     </form>
-                    <button class = "loginBtn">로그인</button>
+                    <button class = "loginBtn">내 호수 생성하기</button>
+                </div>
             </div>
         </div>
-    </div>
+        </div>
   );
 }
 
-export default LoginPage;
+export default SignupPage;
